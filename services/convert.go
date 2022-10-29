2d5ffe6119a01d7296ab235ea1f4cd41231f15fd
saknet <andreas.andra@helsinki.fi>
2022-10-29 17:14:01 +0300

    added to radians

--- services/convert.go
+++ services/convert.go
@@ -8,20 +8,20 @@ import (
 
 func Convert(celestial model.Celstial) model.Cartesian {
 
-	sign := 1
+	sign := 1.0
 
 	if !celestial.Declination.Sign {
-		sign = sign - 2
+		sign = sign - 2.0
 	}
 
 	a := float64(celestial.Right_ascension.Hour)*15 + float64(celestial.Right_ascension.Minute)*0.25 + celestial.Right_ascension.Second*0.004166
-	b := (math.Abs(float64(celestial.Declination.Degree)) + float64(celestial.Declination.Minute)/60 + (celestial.Declination.Second / 3600)) * float64(sign)
+	b := (float64(celestial.Declination.Degree) + float64(celestial.Declination.Minute)/60 + (celestial.Declination.Second / 3600)) * sign
 
 	cartesian := model.Cartesian{}
 	cartesian.Name = celestial.Name
-	cartesian.X = (celestial.Distance * math.Cos(a)) * math.Cos(b)
-	cartesian.Y = (celestial.Distance * math.Cos(b)) * math.Sin(a)
-	cartesian.Z = celestial.Distance * math.Sin(b)
+	cartesian.X = celestial.Distance * math.Cos(toRadians(b)) * math.Cos(toRadians(a))
+	cartesian.Y = celestial.Distance * math.Cos(toRadians(b)) * math.Sin(toRadians(a))
+	cartesian.Z = celestial.Distance * math.Sin(toRadians(b))
 
 	return cartesian
 
@@ -47,6 +47,28 @@ func AddTestObjects() []model.Celstial {
 
 	nearestStars = append(nearestStars, rigilKentaurus)
 
+	procyon := model.Celstial{
+		Name:            "Procyon",
+		Distance:        11.402,
+		Right_ascension: model.Right_ascension{Hour: 7, Minute: 39, Second: 18.11950},
+		Declination:     model.Declination{Degree: 5, Minute: 13, Second: 29.9552, Sign: true},
+	}
+
+	nearestStars = append(nearestStars, procyon)
+
+	tauCeti := model.Celstial{
+		Name:            "Tau Ceti",
+		Distance:        11.912,
+		Right_ascension: model.Right_ascension{Hour: 1, Minute: 44, Second: 4.08338},
+		Declination:     model.Declination{Degree: 15, Minute: 56, Second: 14.9262, Sign: false},
+	}
+
+	nearestStars = append(nearestStars, tauCeti)
+
 	return nearestStars
 
+}
+
+func toRadians(angle float64) float64 {
+	return angle * (math.Pi / 180)
 }
